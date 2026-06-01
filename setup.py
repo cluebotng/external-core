c132b9e769888266f9ca4a7f5a4acc839f7df4ac
Damian Zaremba <damian@damianzaremba.co.uk>
2026-06-01 16:02:08 +0200

    setup - download artifacts from release

--- setup.py
+++ setup.py
@@ -1,23 +1,54 @@
 #!/usr/bin/env python3
-import shutil
+import os
 import subprocess
 from pathlib import PosixPath
 
 
-TARGET_RELEASE = "1.0.3"
-TARGET_PATCH = "3"
+TARGET_RELEASE = "v1.1.0"
 WORKSPACE_DIR = PosixPath("/workspace")
-APT_DIR = PosixPath("/tmp/apt")
-DEB_FILE = PosixPath(f"/tmp/cluebotng-core-{TARGET_RELEASE}-{TARGET_PATCH}.amd64.deb")
 
 
-def install_package():
-    # We do this in a similar style to the `apt` layer as we cannot do a simple `apt-get install`,
-    # basically download the .deb file and extract it directly.
-    # Since we are only interested in the 'static' executable a little fernangle is done.
+def download_release():
+    # Download the binaries
+    for binary in ["cluebotng", "create_ann", "create_bayes_db", "print_bayes_db"]:
+        target_path = WORKSPACE_DIR / binary
+        if not target_path.is_file():
+            subprocess.run(
+                [
+                    "curl",
+                    "--silent",
+                    "--show-error",
+                    "--fail",
+                    "-L",
+                    "-o",
+                    target_path.as_posix(),
+                    f"https://github.com/cluebotng/core/releases/download/{TARGET_RELEASE}/{binary}",
+                ],
+                check=True,
+            )
+            target_path.chmod(0x555)
 
-    # Download the .deb
-    if not DEB_FILE.is_file():
+    # Download the databases
+    (WORKSPACE_DIR / "data").mkdir(parents=True, exist_ok=True)
+    for database in ["main_ann.fann", "bayes.db", "two_bayes.db"]:
+        target_path = WORKSPACE_DIR / "data" / database
+        if not target_path.is_file():
+            subprocess.run(
+                [
+                    "curl",
+                    "--silent",
+                    "--show-error",
+                    "--fail",
+                    "-L",
+                    "-o",
+                    target_path.as_posix(),
+                    f"https://github.com/cluebotng/core/releases/download/{TARGET_RELEASE}/{database}",
+                ],
+                check=True,
+            )
+
+    # Download the config
+    if not (WORKSPACE_DIR / "conf").is_dir():
         subprocess.run(
             [
                 "curl",
@@ -26,56 +57,24 @@ def install_package():
                 "--fail",
                 "-L",
                 "-o",
-                DEB_FILE.as_posix(),
-                "https://launchpad.net/~damianzaremba/+archive/ubuntu/cluebotng-core/+files/"
-                f"cluebotng-core_{TARGET_RELEASE}-{TARGET_PATCH}_amd64.deb",
+                "/tmp/conf.tar.gz",
+                f"https://github.com/cluebotng/core/releases/download/{TARGET_RELEASE}/conf.tar.gz",
             ],
             check=True,
         )
 
-    # Extract the .deb
-    if not (APT_DIR / "usr" / "bin" / "cluebotng").is_file():
         subprocess.run(
             [
-                "dpkg",
-                "-x",
-                DEB_FILE.as_posix(),
-                APT_DIR.as_posix(),
+                "tar",
+                "-C",
+                WORKSPACE_DIR.as_posix(),
+                "-xf",
+                "/tmp/conf.tar.gz",
             ],
             check=True,
         )
 
-    # Copy the install over to the workspace
-    for path in (APT_DIR / "opt" / "cbng-core").iterdir():
-        dst_path = WORKSPACE_DIR / path.name
-        if path.is_file() and not dst_path.exists():
-            shutil.copy(path.as_posix(), dst_path.as_posix())
-        if path.is_dir() and not dst_path.exists():
-            shutil.copytree(path.as_posix(), dst_path.as_posix())
-
-    # Ensure the binaries are executable
-    for binary in ["cluebotng", "create_ann", "create_bayes_db", "print_bayes_db"]:
-        (WORKSPACE_DIR / binary).chmod(0x555)
-
-
-def build_databases():
-    for target, source in [
-        ("bayes.db", "main_bayes_train.dat"),
-        ("two_bayes.db", "two_bayes_train.dat"),
-    ]:
-        subprocess.run(
-            [
-                (WORKSPACE_DIR / "create_bayes_db").as_posix(),
-                (WORKSPACE_DIR / "data" / target).as_posix(),
-                (WORKSPACE_DIR / "data" / source).as_posix(),
-            ],
-            check=True,
-        )
-
-
-def cleanup():
-    shutil.rmtree(APT_DIR.as_posix())
-    DEB_FILE.unlink()
+        os.remove("/tmp/conf.tar.gz")
 
 
 def appease_poetry():
@@ -86,9 +85,7 @@ def appease_poetry():
 
 
 def main():
-    install_package()
-    build_databases()
-    cleanup()
+    download_release()
     appease_poetry()
 
 
